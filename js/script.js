e96333c806263d55e8a5e4cabf50924edda30d3b
tuzin07on <arthurcordao0@gmail.com>
2026-06-08 14:22:55 -0300

    Adição de FAQ e Monitoramento

--- js/script.js
+++ js/script.js
@@ -67,3 +67,22 @@ function simularPlantio(){
         `;
     }
 }
+
+const perguntas = document.querySelectorAll(".faq-question");
+
+perguntas.forEach((pergunta) => {
+
+    pergunta.addEventListener("click", () => {
+
+        const resposta = pergunta.nextElementSibling;
+
+        if(resposta.style.maxHeight){
+            resposta.style.maxHeight = null;
+        }else{
+            resposta.style.maxHeight =
+                resposta.scrollHeight + "px";
+        }
+
+    });
+
+});
