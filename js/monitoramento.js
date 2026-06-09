6339a8f1edbf58904fb6d9d61670692adae631ba
tuzin07on <arthurcordao0@gmail.com>
2026-06-09 13:20:07 -0300

    Ajustes nas pagias de monitoramento

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -6,17 +6,22 @@ function gerarDados() {
   document.getElementById("temp").innerText = temp.toFixed(1);
   document.getElementById("umidade").innerText = Math.floor(umidade);
   document.getElementById("chuva").innerText = Math.floor(chuva);
-  
+
   const statusEl = document.getElementById("status");
+  
   if (umidade > 70 && chuva < 15) {
-    statusEl.innerText = "🌿 FAVORÁVEL";
-    statusEl.style.color = "#22c55e";
+    statusEl.innerHTML = "🌿 FAVORÁVEL";
+    statusEl.style.color = "#52B788";
+    statusEl.style.fontSize = "1.3rem";
   } else if (umidade > 50) {
-    statusEl.innerText = "🔸 ATENÇÃO";
-    statusEl.style.color = "#facc15";
+    statusEl.innerHTML = "⚠️ ATENÇÃO";
+    statusEl.style.color = "#F9C74F";
+    statusEl.style.fontSize = "1.3rem";
   } else {
-    statusEl.innerText = "⚠️ CRÍTICO";
-    statusEl.style.color = "#ef4444";
+    statusEl.innerHTML = "🚨 CRÍTICO";
+    statusEl.style.color = "#E63946";
+    statusEl.style.fontSize = "1.3rem";
   }
 }
-window.onload = gerarDados;
+
+document.addEventListener("DOMContentLoaded", gerarDados);
